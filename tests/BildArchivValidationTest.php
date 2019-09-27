bd204d30f897882a71ce3ff331830ba5cfcbd15d
Till Brede <tb@symcon.de>
2019-09-27 15:50:35 +0200

    added validation test for Bild Archiv (PHP-66)

---
+++ tests/BildArchivValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class BildArchivValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateBildArchiv(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateBildArchivModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../BildArchiv');
+    }
+}
